178daf6db4e3462030f64809558776d0d56ca71a
jhuguespam@gmail.com <jhuguespam@gmail.com>
2023-05-29 23:04:52 +0200

    jhresume

---
+++ components/Content.js
@@ -0,0 +1,23 @@
+import styles from '../styles/Content.module.css';
+
+function Content ({className, title, text, text2, text3, text4, text5, text6, LinkedinText, linkedinLink}) {
+    const contentClassName = `${styles.mainContentContainer} ${className} ${className === styles.projectsTab ? styles.projectsTab : ''}`;
+
+    return (
+        <div className={contentClassName}>
+    {/* // <div className={`${styles.mainContentContainer} ${className}`}> */}
+        <h1 className={styles.mainTitle}>{title}</h1>
+        <p className={styles.ContentText}>{text}</p>
+        <p className={styles.ContentText}>{text2}</p>
+        <p className={styles.ContentText}>{text3}</p>
+        <p className={styles.ContentText}>{text4}</p>
+        <p className={styles.ContentText}>{text5}</p>
+        <p className={styles.ContentText}>{text6}</p>
+        <button className={styles.btnLinkedin}><a href={linkedinLink}className={styles.LinkedinText}>{LinkedinText}</a>
+        </button>
+    </div>
+
+    );
+}
+
+export default Content;
--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,35 @@
 import styles from '../styles/Home.module.css';
+import Content from './Content';
+
+
 
 function Home() {
   return (
-    <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.titleWrapper} >
+        <h1 className={styles.title}>Jean-Hugues </h1>
+        <h1 className={styles.title2}>Pam</h1>
+        <p className={styles.text}>Développeur full stack React JS | React Native |</p>
+        <p className={styles.text}>Spécialiste communication digitale.</p>
+        <button className={styles.btn}><a href='mailto:jhuguespam@gmail.com'className={styles.mailto}>Email Me</a></button>
+        </div >
+        <div className={styles.picContainer}>
+        <img src='TyeCeePic.jpg' alt='Photographie de JH' className={styles.mainPicture} />
+        </div>
+        <Content className={styles.content} title='Compétences Professionnelles' text='Création, développement & coordination de projets digitaux : applications web & mobiles, e-magazines, e-mailing responsives. JavaScript, TypeScript,' 
+        text2='Node.JS, HTML, CSS, React JS,React Native, Express, Redux, MongoDB, Git, Git Hub, VS Code'
+        text3='Graphisme et PAO : Photoshop, Illustrator, InDesign, Figma, Excalidraw.'
+        text4='Montage video : Premiere Pro, After Effects, iMovie'
+        text5='Définition et mise en oeuvre de stratégies de communication digitale à destination d’audiences'
+        text6='ciblées, communication de crise, animation de réseaux sociaux.'
+        LinkedinText='Linkedin'
+        linkedinLink='https://www.linkedin.com/in/jean-hugues-pam/'
+        />
+        <Content className={styles.projectsTab} title='Mes Projets' text='Consultez mes projets sur GitHub'
+        LinkedinText='GitHub'
+        linkedinLink='https://github.com/jhty75'/>
       </main>
-    </div>
+
   );
 }
 
