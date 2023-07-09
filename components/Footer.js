66cfaa06a8b98715fcffcbb96b1c2344c97b3c4c
jhuguespam@gmail.com <jhuguespam@gmail.com>
2023-07-09 17:08:08 +0200

    myresume

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,12 @@
 import styles from '../styles/Footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpaceAwesome } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
         <div className={styles.mainContainer}>
             <div className={styles.title}>
-                <p>© 2023 - Made by <a href='mailto:jhuguespam@gmail.com' className={styles.jhpLink}>JHP</a></p>
+                <p>© 2023 - Made by <a href='mailto:jhuguespam@gmail.com' className={styles.jhpLink}>JHP</a> <FontAwesomeIcon icon={faSpaceAwesome} style={{color :"white", transform: 'rotate(30deg)', marginLeft: '5px'}}/> </p>
             </div>
         </div>
     )
