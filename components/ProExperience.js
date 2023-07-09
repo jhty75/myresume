5a10b38c3ccfbc93e51c948eeb9469f481213f57
jhuguespam@gmail.com <jhuguespam@gmail.com>
2023-07-09 16:03:53 +0200

    myresume

--- components/ProExperience.js
+++ components/ProExperience.js
@@ -19,12 +19,12 @@ function ProExperience() {
             </div>
             <p className={styles.jobPlaces}>British Embassy Paris |
                 De 2020 à ce jour</p>
-            <p className={styles.jobMission}>Publication et mise à jour du site internet <a href='https://www.gov.uk'>GOV.UK</a> (6M de visiteurs/jour) | Rédaction, implémentation et évaluation de
+            <p className={styles.jobMission}>Publication et mise à jour du site internet <a href='https://www.gov.uk'>GOV.UK</a> (6M de visiteurs/jour) | Création et conduite de sessions de formation à la
+                communication digitale et renforcement de la capacité des équipes | Rédaction, implémentation et évaluation de
                 la stratégie social media | Création de contenu bilingue (vidéos, photographies,
                 infographies, campagnes publicitaires) et gestion des réseaux sociaux |
                 Production de la revue de presse hebdomadaire et diffusion au équipes
-                internationales | Création et conduite de sessions de formation à la
-                communication digitale et renforcement de la capacité des équipes |
+                internationales |
                 Participation aux cellules de crises et rédaction rapports associés
                 (COVID-19, Brexit, Ukraine) </p>
             <p className={styles.jobMission}><strong>Résultats :</strong> Augmentation du nombre de followers : 73% en moyenne par an et accroissement du taux d’engagement de 9,55% par an en moyenne toutes nos plateformes confondues.
@@ -34,12 +34,15 @@ function ProExperience() {
             </div>
             <p className={styles.jobPlaces}>Free-Iliad, à Paris |
                 De 2016 à 2020</p>
-            <p className={styles.jobMission}>Redéfinition ligne éditoriale et design graphique du e-magazine interne mensuel | Gestion calendrier éditorial annuel |
-                Rédaction articles | diffusion de campagnes d’emailing | Création supports de communication digitale, videos, goodies | Lancement, animation et modération
-                de la digitale Workplace by Facebook | Production vidéos corporates | Référent RQTH | Enquêtes d’opinion interne | Pilotage « Culture Audit » GPTW | Gestion des budgets.</p>
+            <p className={styles.jobMission}>RRefonte intégrale et gestion du e-magazine interne mensuel | Gestion
+                de projets avec l’équipe IT pour développement d’applications de diffusion
+                de campagnes d’e-mailing responsive et d’outils de statistiques | Lancement, animation et modération
+                de la digitale Workplace | Production
+                de vidéos | Développement de formation digitale |  Identification, négociation et coordination
+                prestataires / Gestion des budgets / Enquêtes d’opinion interne | Pilotage « Culture Audit » GPTW.</p>
             <div>
                 <p className={styles.jobMission}><strong>Résultats :</strong> Augmentation audience magazine (63% par mois en moyenne) |
-                    Meilleur usage par les employés de la digitale Workplace | Entreprise certifiée GPTW (71% des collaborateurs considèrent
+                    Meilleur usage par les employés de la digitale Workplace | Obtention de la certification GPTW (71% des collaborateurs considèrent
                     qu’il fait bon travailler dans l’entreprise). </p>
             </div>
             <div>
@@ -48,14 +51,13 @@ function ProExperience() {
             </div>
             <p className={styles.jobPlaces}>Adagio Advising, à Paris |
                 De 2008 à 2015</p>
-            <p className={styles.jobMission}>Gestion et développement de projets digitaux | Création de sites Internet
+            <p className={styles.jobMission}>GGestion et développement de projets digitaux : Création de sites Internet
                 et administration via CMS | Définition et implementation stratégie de
-                contenus, gestion de blog et réseaux sociaux / SEO & SEA / Création de
+                contenus, gestion de blog et réseaux sociaux | SEO & SEA | Création de
                 campagnes de Marketing Direct | Rédaction de dossier de partenariat |
-                Développement et Consolidation d’un réseau de prescripteurs |
-                Veille concurrentielle | Gestion Relation Clients - Réalisation d’enquêtes
-                de satisfaction clients | Publication d’offres d’emploi sur jobboards |
-                Organisation sessions recrutement | Suivi du budget.</p>
+                Développement et Consolidation d’un réseau de prescripteurs /
+                Veille concurrentielle | Gestion Relation Clients | Réalisation d’enquêtes
+                de satisfaction clients | Suivi du budget.</p>
             <div>
                 <p className={styles.jobMission}><strong>Résultats :</strong> Progression CA de 33% par an en moyenne |
                     Développement du portefeuille clients de 45% par an en moyenne |
@@ -66,12 +68,12 @@ function ProExperience() {
             </div>
             <p className={styles.jobPlaces}>Consulat général de France à Miami, USA |
                 2007 </p>
-            <p className={styles.jobMission}>Publication et mise à jour du site Internet | Organisation concerts (artistes SONY MUSIC)
-                Développement d’un partenariat avec UNIVERSAL MUSIC et pilotage du
-                projet Make Music Miami! | Coordination des prestataires, de la logistique
-                événementielle | Rédaction et création supports de communication
-                bilingues | Gestion des Relations Presse | Veille, traitement de l’information
-                culturelle locale et élaboration de la revue de presse.</p>
+            <p className={styles.jobMission}>PPublication et mise à jour de la rubrique du site Internet | Développement
+                d’un partenariat avec UNIVERSAL MUSIC et pilotage du projet Make Music
+                Miami! | Coordination des prestataires, de la logistique événementielle |
+                Rédaction et création supports de communication bilingues | Veille,
+                traitement de l’information culturelle locale et élaboration de la revue de
+                presse.</p>
             <div>
                 <p className={styles.jobMission}><strong>Résultats :</strong> Valorisation Culture française à l’international |
                     Reconduction des partenariats et du projet Make Music Miami! </p>
