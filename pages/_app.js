970942303cc621eeae3720fe212a905347a19b2e
jhuguespam@gmail.com <jhuguespam@gmail.com>
2023-07-13 14:58:29 +0200

    myresume

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,11 @@
 import '../styles/globals.css';
-import ReactGA from 'react-ga';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Head from 'next/head';
-
-
-const TRACKING_ID = "G-CMDHENFJ9K"; // my Google tracking ID
-ReactGA.initialize(TRACKING_ID);
-
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 
 function App({ Component, pageProps }) {
-
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ReactGA.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, []);
-
-
   return (
     <>
-      <Head>
-        <title></title>
-      </Head>
+     <GoogleAnalytics trackPageViews gaMeasurementId="G-CMDHENFJ9K" />
       <Component {...pageProps} />
     </>
   );
